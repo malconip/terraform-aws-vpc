594b7b03e14bbda9632ae5a94a2cf3aa5d13dec1
Malcon Albuquerque Moreira <malconip@outlook.com>
2022-06-01 19:17:35 -0300

    Adding remote tfstate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,5 +11,23 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = "AKIA5XYOJQYUM66PC4CQ"
+  secret_key = "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"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-aws-vpc-state"
+    key    = "default-infrastructure"
+    region = var.region
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-aws-vpc-state"
+
+  versioning {
+    enabled = true
+  }
 }
