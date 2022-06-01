2f663eda20feda592b26f3b3327a165d5e87318f
Malcon Albuquerque Moreira <malconip@outlook.com>
2022-06-01 18:18:33 -0300

    Creating 2 subnets

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -7,3 +7,12 @@ resource "aws_subnet" "prod-subnet-public-1" {
     Name = "prod-subnet-public-1"
   }
 }
+resource "aws_subnet" "prod-subnet-public-2" {
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "prod-subnet-public-2"
+  }
+}
--- terraform/routes.tf
+++ terraform/routes.tf
@@ -16,3 +16,8 @@ resource "aws_route_table_association" "prod-crta-public-subnet-1" {
   subnet_id      = aws_subnet.prod-subnet-public-1.id
   route_table_id = aws_route_table.prod-public-crt.id
 }
+
+resource "aws_route_table_association" "prod-crta-public-subnet-2" {
+  subnet_id      = aws_subnet.prod-subnet-public-2.id
+  route_table_id = aws_route_table.prod-public-crt.id
+}
