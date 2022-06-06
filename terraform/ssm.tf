5f9cf4b361f8ada7134ad8b0b964c7ec7b3e1006
Malcon Albuquerque Moreira <malconip@outlook.com>
2022-06-06 18:35:40 -0300

    Adding subnet parameters

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "sg" {
+resource "aws_ssm_parameter" "vpc-id" {
   name        = "/vpc/vpc-id"
   description = "Id of the vpc"
   type        = "String"
@@ -10,5 +10,35 @@ resource "aws_ssm_parameter" "sg" {
   }
   depends_on = [
     aws_vpc.prod-vpc
+  ]
+}
+
+resource "aws_ssm_parameter" "ssm-prod-subnet-public-1" {
+  name        = "/vpc/prod-subnet-public-1"
+  description = "Id of the prod-subnet-public-1"
+  type        = "String"
+  value       = aws_subnet.prod-subnet-public-1.id
+
+  tags = {
+    environment = "prod"
+
+  }
+  depends_on = [
+    aws_subnet.prod-subnet-public-1
+  ]
+}
+
+resource "aws_ssm_parameter" "ssm-prod-subnet-public-2" {
+  name        = "/vpc/prod-subnet-public-2"
+  description = "Id of the prod-subnet-public-2"
+  type        = "String"
+  value       = aws_subnet.prod-subnet-public-2.id
+
+  tags = {
+    environment = "prod"
+
+  }
+  depends_on = [
+    aws_subnet.prod-subnet-public-2
   ]
 }
